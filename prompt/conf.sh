876f6ab17c6bd6cad16c2774892784cbdd917963
Trevor Lemon <trevor.lemon@nike.com>
2016-05-18 11:30:11 -0700

    Added custom prompt
    I've been running this custom prompt for a while, so I decided to add it to VC.

---
+++ prompt/conf.sh
@@ -0,0 +1,16 @@
+# == setup history sharing between prompts ==========================
+# Where it gets saved
+HISTFILE=~/.history
+# Remember about a years worth of history (AWESOME)
+SAVEHIST=10000
+HISTSIZE=10000
+# Don't overwrite, append!
+setopt APPEND_HISTORY
+# Killer: share history between multiple shells
+setopt SHARE_HISTORY
+# If I type cd and then cd again, only save the last one
+setopt HIST_IGNORE_DUPS
+# Even if there are commands inbetween commands that are the same, still only save the last one
+setopt HIST_IGNORE_ALL_DUPS
+# Pretty    Obvious.  Right?
+setopt HIST_REDUCE_BLANKS
