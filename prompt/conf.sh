9a9c5525cd2e0a0cabf73aaa1230bbbb60f32533
Trevor Lemon <trevor.lemon@nike.com>
2016-07-05 16:48:40 -0700

    Added history conf option

--- prompt/conf.sh
+++ prompt/conf.sh
@@ -12,6 +12,8 @@ setopt SHARE_HISTORY
 setopt HIST_IGNORE_DUPS
 # Even if there are commands inbetween commands that are the same, still only save the last one
 setopt HIST_IGNORE_ALL_DUPS
+# Use this locking method to prevent errors on initial start
+setopt HIST_FCNTL_LOCK
 # Pretty    Obvious.  Right?
 setopt HIST_REDUCE_BLANKS
 
