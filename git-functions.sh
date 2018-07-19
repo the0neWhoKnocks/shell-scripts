141fa87c75f56549ac27a2b2955288d3d477ea0c
Nox <the0neWhoKnocks@users.noreply.github.com>
2018-07-19 10:42:11 -0700

    Use apply instead of pop

--- git-functions.sh
+++ git-functions.sh
@@ -16,7 +16,7 @@ source $(dirname $0)/colors.sh
 # This is the branch that serves as the default root branch
 # usually this'd be 'master', but in some projects it may be
 # different. It's primarily used in the case where you try to
-# delete a branch you're currently in. You will be moved to 
+# delete a branch you're currently in. You will be moved to
 # this branch, allowing the previous branch to be deleted.
 gitProjectRoot="master"
 
@@ -56,7 +56,7 @@ function git-update () {
   if [[ "$thereAreChanges" != "" ]]; then
     echo;
     echo -e " ${BCya}[UN-STASH]${RCol} changes"
-    git stash pop
+    git stash apply
   fi
 }
 alias gu='git-update'
@@ -88,7 +88,7 @@ function git-changes (){
 
     # if the status is set, add a space and proper coloring
     if [ "$stat" != "" ]; then
-      # ${red}$stat${reset} 
+      # ${red}$stat${reset}
       modifiedFiles=$(echo -e "$modifiedFiles${Cya}$file$fp ${BBla}")
       modifiedCount=$((modifiedCount+1))
     elif [ "$file" != "" ]; then
@@ -101,7 +101,7 @@ function git-changes (){
     fi
 
   done < <(git status -s)
-  
+
   echo -e "\n\n";
   echo -e "${BBla}████████████████████████████████████████████"
   echo -e "\n Modified Files ${RCol}($modifiedCount)${BBla} \n"
@@ -153,17 +153,17 @@ function git-add-branch () {
         ;;
       esac
     fi
-    
+
     local parentBranch=$(git rev-parse --abbrev-ref HEAD)
 
     echo;
     echo -e " ${BCya}[CREATING]${RCol} local branch ${BYel}$branchName${RCol}"
     git checkout -b $branchName
-    
+
     echo;
     echo -e " ${BCya}[ADDING]${RCol} ${BYel}$branchName${RCol} to your remote origin"
     git push --no-verify -u origin $branchName
-    
+
     echo;
     echo -e " ${BCya}[SETTING]${RCol} the remote for ${BYel}$branchName${RCol} to origin"
     git config "branch.$branchName.remote" "origin"
@@ -200,19 +200,19 @@ function git-delete-branch () {
       echo -e " ${BCya}[STASH]${RCol} changes"
       git stash
     fi
-    
+
     if [[ "$currBranch" == "$1" ]]; then
       # check if the branch exists before switching
       git show-ref --verify --quiet refs/heads/$parentBranch
       if [[ "$?" == "1" ]]; then
         echo;
         echo -e " ${BRed}[ERROR]${RCol} The branch assigned to parentBranch:${BYel}$parentBranch${RCol} doesn't exist locally."
-        
+
         if [[ "$thereAreChanges" != "" ]]; then
           echo -e " ${BCya}[UN-STASH]${RCol} changes"
-          git stash pop
+          git stash apply
         fi
-          
+
         okToProceed=0
       else
         echo;
@@ -222,23 +222,23 @@ function git-delete-branch () {
         git checkout $parentBranch
       fi
     fi
-    
+
     if [ $okToProceed -eq 1 ]; then
       echo;
       echo -e " ${BCya}[DELETING]${RCol} ${BYel}$1${RCol} from remote origin"
       git push origin --delete --no-verify $1
-      
+
       echo;
       echo -e " ${BCya}[DELETING]${RCol} ${BYel}$1${RCol} from your local repo"
       git branch -D $1
-      
+
       if [[ "$thereAreChanges" != "" ]]; then
         echo;
         echo -e " ${BCya}[UN-STASH]${RCol} changes"
-        git stash pop
+        git stash apply
       fi
     fi
-      
+
   else
     echo;
     echo "usage: git-delete-branch <branch-name>"
@@ -249,9 +249,9 @@ alias gdb='git-delete-branch'
 
 ##
 # There are use cases where you'll be changing versioned files but you never
-# want them committed. This allows you to easily add, remove, or list files 
+# want them committed. This allows you to easily add, remove, or list files
 # with the assume-unchanged flag.
-# 
+#
 # - mark versioned files as 'assume-unchanged'
 # - remove the 'assume-unchanged' flag from a file
 # - list files marked as 'assume-unchanged'
@@ -328,7 +328,7 @@ function git-patch () {
     echo;
     return 0;
   }
-  
+
   while [[ "$*" ]]; do
     case $1 in
       "-c"|"--commit-count")
@@ -345,7 +345,7 @@ function git-patch () {
       ;;
     esac
   done
-  
+
   # Throw error if no file name passed.
   if [ -z ${fileName+x} ]; then
     echo;
@@ -354,11 +354,11 @@ function git-patch () {
   else
     local patchName="$fileName.patch"
   fi
-  
+
   # If no commit number set, check if there are un-staged changes and patch those.
   if [ -z ${commitCount+x} ]; then
     local diff=$(git diff);
-    
+
     if [[ "$diff" != "" ]]; then
       git diff --no-color > "$patchName"
       echo;
@@ -370,7 +370,7 @@ function git-patch () {
     fi
   else
     [[ "$commitCount" == "1" ]] && s="" || s="s"
-    
+
     git format-patch HEAD~$commitCount --no-stat --stdout > "$patchName"
     echo;
     echo -e " ${BCya}[CREATED]${RCol} $patchName from $commitCount commit$s"
@@ -431,7 +431,7 @@ function git-squash () {
       echo;
       echo -e " ${BCya}[STASH]${RCol} changes"
       echo;
-      
+
       git stash
     fi
 
@@ -440,9 +440,9 @@ function git-squash () {
     if [[ "$thereAreChanges" != "" ]]; then
       echo;
       echo -e " ${BCya}[UN-STASH]${RCol} changes"
-      git stash pop
+      git stash apply
     fi
-    
+
     echo;
     echo "If something went wrong during the squash, just run 'git-undo-squash' or 'gus'"
     echo;
@@ -456,11 +456,11 @@ function git-squash () {
     echo "Do you want to view the logs?"
     echo " Y / N"
     echo;
-    
+
     # capture the user's choice
     echo -n ""
     read selectedOption
-    
+
     if [[ "$selectedOption" == "y" || "$selectedOption" == "Y" ]]; then
       git log --stat --pretty=format:" %C(yellow)%h%C(reset) - %C(cyan)%aN%C(reset) : %s"
     fi
@@ -478,22 +478,22 @@ function git-undo-squash () {
   echo "Do you want to view the reflog?"
   echo " Y / N"
   echo;
-  
+
   # capture the user's choice
   echo -n ""
   read selectedOption
-  
+
   if [[ "$selectedOption" == "y" || "$selectedOption" == "Y" ]]; then
     git reflog
-    
+
     echo;
     echo "Enter in the HEAD revision number or type 'exit'"
     echo;
-    
+
     # capture the user's choice
     echo -n ""
     read revisionNumber
-    
+
     if [[ "$revisionNumber" == "exit" ]]; then
       exit 0;
     else
@@ -507,7 +507,7 @@ alias gus='git-undo-squash'
 # Displays the number of commits for all users or by a specified user
 function git-brag () {
   local proceed=true
-  
+
   function bragUsage () {
     echo;
     echo " usage: git-brag [-h] [-u <user_name>]"
@@ -521,7 +521,7 @@ function git-brag () {
     echo;
     proceed=false
   }
-  
+
   while getopts ":hu:" opt; do
     case $opt in
       h)
@@ -543,24 +543,24 @@ function git-brag () {
       ;;
     esac
   done
-  
+
   if $proceed ; then
     if [[ "$userName" != "" && "$userName" != "%n" ]]; then
       local totalCommits=0
-      
+
       echo;
-      
+
       while read line; do
         # This is pretty slow currently and should be refactored
         local ptrnMatch=$(echo "$line" | grep -io "$userName")
-        
+
         if [[ "$ptrnMatch" != "" ]]; then
           echo " ${BCya}[FOUND]${RCol} $line"
           local count=$(echo "$line" | grep -o "^[0-9]*");
           let "totalCommits += $count"
         fi
       done < <(git shortlog -s -n)
-      
+
       echo;
       echo " The user matching ${BYel}$userName${RCol} has ${BGre}$totalCommits${RCol} commits"
       echo;
@@ -588,9 +588,9 @@ function git-undo-add () {
     echo;
     echo -e " ${BCya}[UN-STAGING]${RCol} ${BYel}$1${RCol}"
     echo;
-    
+
     git reset HEAD "$1"
-    
+
   else
     echo;
     echo "usage: git-undo-add <file-or-folder-name>"
@@ -608,7 +608,7 @@ function git-undo-commit () {
   echo;
   echo -e " ${BCya}[RESETTING]${RCol} last commit"
   echo;
-  
+
   git reset --soft HEAD~1
 }
 alias guc='git-undo-commit'
@@ -620,9 +620,9 @@ function git-restore-file () {
     echo;
     echo -e " ${BCya}[RESTORING]${RCol} ${BYel}$1${RCol}"
     echo;
-    
+
     git checkout $(git rev-list -n 1 HEAD -- "$1")~1 -- "$1"
-    
+
   else
     echo;
     echo "usage: git-restore-file <full-path-to-file>"
@@ -640,7 +640,7 @@ function git-reset-perms () {
   echo;
   echo -e " ${BCya}[RESETTING]${RCol} file permissions"
   echo;
-  
+
   git diff -p -R --no-color \
     | grep -E "^(diff|(old|new) mode)" --color=never \
     | git apply -v
