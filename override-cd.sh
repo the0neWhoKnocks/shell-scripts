4a3838f9d4fbaebac2a7aef0f3f0116c87fccdc4
Nox <the0neWhoKnocks@users.noreply.github.com>
2022-12-20 12:18:31 -0800

    feat[override-cd]: Refinements
    - Created a logger to more easily debug any issues. Can be enabled with `export DEBUG_CD=true`.
    - Switched from aliases to functions in the files being sourced. That file now exports an Array so I can print out what's added, and also so I can remove them.

--- override-cd.sh
+++ override-cd.sh
@@ -1,18 +1,50 @@
+#!/bin/bash
+
 function cd () {
+  function log () {
+    if [ -n "$DEBUG_CD" ]; then
+      echo "[cd] $1"
+    fi
+  }
+  
+  log "Called with: '$@'"
   builtin cd "$@" # perform the actual cd
   
+  log "Current directory: '$PWD'"
+  inApps=false
   case "$PWD" in
-    *"Projects/Code/Apps"*) inApps=true ;;
+    *"Projects/Code/Apps/"*) inApps=true ;;
     *) ;;
   esac
-
-  if [ -n "$inApps" ]; then
-    # echo "in apps dir"
-    if [ -d ".git" ] && [ -f "./bin/aliases.sh" ]; then
-      source "./bin/aliases.sh"
-      echo "[cd] Sourced aliases for app repo"
+  
+  if [ "$inApps" = true ]; then
+    log "In required dir"
+    
+    repoFile="${PWD}/bin/repo-funcs.sh"
+    if [ -d ".git" ] && [ -f "${repoFile}" ]; then
+      log "  Sourcing: ${repoFile}"
+      source "${repoFile}"
+      echo "╭───────"
+      for currFunc in "$REPO_FUNCS[@]"; do
+        echo "│[ADDED] Function: '${currFunc}'"
+      done
+      echo "╰───────"
+      
+      log "  Done"
     fi
   else
-    # echo "not in apps dir"
+    log "Not in required dir"
+    
+    if [ -n "$REPO_FUNCS" ]; then
+      echo "╭─────────"
+      for currFunc in "$REPO_FUNCS[@]"; do
+        unset -f $currFunc
+        echo "│[REMOVED] Function: '${currFunc}'"
+      done
+      echo "╰─────────"
+      
+      unset REPO_FUNCS
+      log "Clean-up for repo complete"
+    fi
   fi
 }
