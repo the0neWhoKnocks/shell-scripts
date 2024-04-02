4853738039d7718ab49a78ad2c5a2d5633c460bc
Nox <the0neWhoKnocks@users.noreply.github.com>
2024-04-02 11:09:07 -0700

    fix[prev-dir]: VSCode can't detect Shell and crashes
    The prompt was waiting for a User to choose a folder or not, and in this case that User was a non-interactive VSCode script. Not sure why their script eats up resources until it eventually crashes though.

--- prev-dir.sh
+++ prev-dir.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-##
-# store previous session data
-onExit() {
-  if [[ "${PWD}" != "${HOME}" ]]; then
-    echo "export CUSTOM__PREV_DIR='${PWD}'" > ~/.prev_term
-  fi
-}
+# Only run within an interactive context
+if [ -t 0 ]; then
+  ##
+  # store previous session data
+  onExit() {
+    if [[ "${PWD}" != "${HOME}" ]]; then
+      echo "export CUSTOM__PREV_DIR='${PWD}'" > ~/.prev_term
+    fi
+  }
 
-trap onExit EXIT
+  trap onExit EXIT
 
-if [ -f ~/.prev_term ]; then
-  source ~/.prev_term
-  
-  # only prompt to load previous dir if one has been saved, and a new shell was started
-  if [[ "${CUSTOM__PREV_DIR}" != "" ]] && [[ "${PWD}" == "${HOME}" ]]; then  
-    while true; do
-      read "yn?Load '${CUSTOM__PREV_DIR}' (y/n)?: "
-      case $yn in
-        [Yy]* )
-          cd "${CUSTOM__PREV_DIR}"
-          break
-          ;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-      esac
-    done
+  if [ -f ~/.prev_term ]; then
+    source ~/.prev_term
+    
+    # only prompt to load previous dir if one has been saved, and a new shell was started
+    if [[ "${CUSTOM__PREV_DIR}" != "" ]] && [[ "${PWD}" == "${HOME}" ]]; then  
+      while true; do
+        read "yn?Load '${CUSTOM__PREV_DIR}' (y/n)?: "
+        case $yn in
+          [Yy]* )
+            cd "${CUSTOM__PREV_DIR}"
+            break
+            ;;
+          [Nn]* ) break;;
+          * ) echo "Please answer yes or no.";;
+        esac
+      done
+    fi
   fi
 fi
+
